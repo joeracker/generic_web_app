900dae605b457db72a29a9252ff0c0f076abf631
Joe <joe@mejoe.com>
2014-03-15 23:09:58 -0500

    fix problem accessing forwarded port on host, why does 0.0.0.0 work but 127.0.0.1 doesn't?

--- generic.py
+++ generic.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+#    return render_template('index.html')
+    return "hello worldss"
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    SERVER_NAME = "0.0.0.0"
+    SERVER_PORT = 5000
+
+    app.run(SERVER_NAME, SERVER_PORT)
+
+    #app.run()
+foo
